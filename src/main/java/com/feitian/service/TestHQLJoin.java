ee9de430652222c5a6f4256e961c8a111add89b7
feitian <843996223@qq.com>
2018-04-20 17:29:44 +0800

    version 20180420

---
+++ src/main/java/com/feitian/service/TestHQLJoin.java
@@ -0,0 +1,63 @@
+package com.feitian.service;
+
+import com.feitian.pojo.Dept;
+import com.feitian.pojo.User;
+import com.feitian.utils.HibernateUtil;
+import org.hibernate.HibernateError;
+import org.hibernate.Transaction;
+
+import java.util.List;
+
+/**
+ * @Author: feitian
+ * @Date: 2018-04-20  16:44
+ * @description:
+ */
+public class TestHQLJoin {
+
+    //左外连接
+    public void testLeftJoin(){
+        Transaction tx = null;
+        try {
+            tx = HibernateUtil.getCurrentSession().beginTransaction();
+
+            String hql = "from Dept d left join d.users";
+            //得出的集合里面是一个数组类型，一个数组为一个dept 和 user
+            //内连接 右外连接一样的结果集
+            List<Object[]> obj = HibernateUtil.getCurrentSession().createQuery(hql).list();
+            for (Object[] ob:obj) {
+                System.out.println(((Dept)ob[0]).getName() + " " + ((Dept)ob[0]).getDeptNo());
+                System.out.println(" " +((User)ob[1]).getName());
+            }
+            tx.commit();
+        }catch (HibernateError error){
+            error.printStackTrace();
+            if (tx!=null){
+                tx.rollback();
+            }
+        }
+    }
+
+    /**
+     * 迫切左外连接
+     */
+    public void testLeftJoinFetch(){
+        Transaction tx = null;
+        try {
+            tx = HibernateUtil.getCurrentSession().beginTransaction();
+
+            String hql = "from Dept d left join fetch d.users";
+            //迫切左外连接 连接后 ，user对象自动填充到dept对象的set属性中，得到的结果是dept对象的集合
+            List<Dept> obj = HibernateUtil.getCurrentSession().createQuery(hql).list();
+            for (Dept ob:obj) {
+                System.out.println(ob.getName());
+            }
+            tx.commit();
+        }catch (HibernateError error){
+            error.printStackTrace();
+            if (tx!=null){
+                tx.rollback();
+            }
+        }
+    }
+}
---
+++ src/test/java/com/feitian/service/testHQLJoinTest.java
@@ -0,0 +1,23 @@
+package com.feitian.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @Author: feitian
+ * @Date: 2018-04-20  16:53
+ * @description:
+ */
+public class testHQLJoinTest {
+
+    @Test
+    public void testLeftJoin() {
+        new TestHQLJoin().testLeftJoin();
+    }
+
+    @Test
+    public void testLeftJoinFetch() {
+        new TestHQLJoin().testLeftJoinFetch();
+    }
+}
